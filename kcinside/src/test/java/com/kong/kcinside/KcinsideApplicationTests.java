ca11e14e749155a5eb963302a57ea56e1d022e5e
USER <USER@DESKTOP-LID3CBQ>
2024-02-26 15:20:15 +0900

    1

---
+++ kcinside/src/test/java/com/kong/kcinside/KcinsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kong.kcinside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcinsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
